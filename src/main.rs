0dc193fa27cf2fffbf7b36e9ff02ab53268beba5
kx0101 <liakos.koulaxis@yahoo.com>
2026-09-16 00:05:31 +0300

    ta commit messages einai gia klamata really

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{self, Write};
+use std::io::{self, Stdout, Write};
 
 #[derive(Debug)]
 enum Role {
@@ -21,6 +21,46 @@ struct Message {
     content: String,
 }
 
+#[derive(Debug, PartialEq)]
+enum Command {
+    History,
+    Clear,
+}
+
+fn get_command(input: &str) -> Option<Command> {
+    if input.trim_end() == "/history" {
+        return Some(Command::History);
+    }
+
+    if input.trim_end() == "/clear" {
+        return Some(Command::Clear);
+    }
+
+    None
+}
+
+fn get_history(conversation: &Vec<Message>, stdout: &mut Stdout) -> Result<(), std::io::Error> {
+    for line in conversation {
+        let role = &line.role;
+        let content = &line.content;
+
+        let line = format!("{role}{content}");
+        stdout.write_all(line.as_bytes())?;
+        stdout.write_all(b"\n")?;
+    }
+
+    Ok(())
+}
+
+fn clear(conversation: &mut Vec<Message>, stdout: &mut Stdout) -> Result<(), std::io::Error> {
+    conversation.clear();
+
+    stdout.write_all(b"conversation cleared")?;
+    stdout.write_all(b"\n")?;
+
+    Ok(())
+}
+
 fn main() -> Result<(), std::io::Error> {
     let stdin = io::stdin();
     let mut stdout = io::stdout();
@@ -38,34 +78,24 @@ fn main() -> Result<(), std::io::Error> {
             return Ok(());
         }
 
+        match get_command(input) {
+            Some(Command::History) => {
+                get_history(&conversation, &mut stdout)?;
+                continue;
+            }
+            Some(Command::Clear) => {
+                clear(&mut conversation, &mut stdout)?;
+                continue;
+            }
+            None => {
+                if input.trim_end().is_empty() {
+                    continue;
+                }
+            }
+        };
+
         if input.trim_end() == "exit" {
             return Ok(());
-        }
-
-        if input.trim_end() == "/history" {
-            for line in &conversation {
-                let role = &line.role;
-                let content = &line.content;
-
-                let line = format!("{role}{content}");
-                stdout.write_all(line.as_bytes())?;
-                stdout.write_all(b"\n")?;
-            }
-
-            continue;
-        }
-
-        if input.trim_end() == "/clear" {
-            conversation.clear();
-
-            stdout.write_all(b"conversation cleared")?;
-            stdout.write_all(b"\n")?;
-
-            continue;
-        }
-
-        if input.trim().is_empty() {
-            continue;
         }
 
         stdout.write_all(input.as_bytes())?;
@@ -81,5 +111,31 @@ fn main() -> Result<(), std::io::Error> {
             content: input.trim().to_string(),
         };
         conversation.push(forge_answer);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_command_history() {
+        let input = "/history";
+        let command = get_command(input).unwrap();
+        assert_eq!(command, Command::History);
+    }
+
+    #[test]
+    fn test_get_command_clear() {
+        let input = "/clear";
+        let command = get_command(input).unwrap();
+        assert_eq!(command, Command::Clear);
+    }
+
+    #[test]
+    fn test_get_command_returns_none() {
+        let input = "whats up there";
+        let command = get_command(input);
+        assert_eq!(command, None);
     }
 }

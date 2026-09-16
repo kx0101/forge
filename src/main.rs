59d32532d761acc9c09dac26ffeae757daa3183a
kx0101 <liakos.koulaxis@yahoo.com>
2026-09-17 00:01:14 +0300

    make repl testable giati kahkame

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::io::{self, Stdout, Write};
+use std::io::Write;
+use std::io::{self, BufRead};
 
 #[derive(Debug)]
 enum Role {
@@ -39,52 +40,59 @@ fn get_command(input: &str) -> Option<Command> {
     None
 }
 
-fn get_history(conversation: &Vec<Message>, stdout: &mut Stdout) -> Result<(), std::io::Error> {
+fn get_history<W>(conversation: &Vec<Message>, writer: &mut W) -> Result<(), std::io::Error>
+where
+    W: Write,
+{
     for line in conversation {
         let role = &line.role;
         let content = &line.content;
 
         let line = format!("{role}{content}");
-        stdout.write_all(line.as_bytes())?;
-        stdout.write_all(b"\n")?;
+        writer.write_all(line.as_bytes())?;
+        writer.write_all(b"\n")?;
     }
 
     Ok(())
 }
 
-fn clear(conversation: &mut Vec<Message>, stdout: &mut Stdout) -> Result<(), std::io::Error> {
+fn clear<W>(conversation: &mut Vec<Message>, writer: &mut W) -> Result<(), std::io::Error>
+where
+    W: Write,
+{
     conversation.clear();
 
-    stdout.write_all(b"conversation cleared")?;
-    stdout.write_all(b"\n")?;
+    writer.write_all(b"conversation cleared")?;
+    writer.write_all(b"\n")?;
 
     Ok(())
 }
 
-fn main() -> Result<(), std::io::Error> {
-    let stdin = io::stdin();
-    let mut stdout = io::stdout();
+fn run<R, W>(reader: &mut R, writer: &mut W) -> Result<(), std::io::Error>
+where
+    R: BufRead,
+    W: Write,
+{
     let input = &mut String::new();
-
     let mut conversation: Vec<Message> = Vec::new();
 
     loop {
-        stdout.write_all(b"forge> ")?;
-        stdout.flush()?;
+        writer.write_all(b"forge> ")?;
+        writer.flush()?;
 
         input.clear();
-        let bytes_read = stdin.read_line(input)?;
+        let bytes_read = reader.read_line(input)?;
         if bytes_read == 0 {
             return Ok(());
         }
 
         match get_command(input) {
             Some(Command::History) => {
-                get_history(&conversation, &mut stdout)?;
+                get_history(&conversation, writer)?;
                 continue;
             }
             Some(Command::Clear) => {
-                clear(&mut conversation, &mut stdout)?;
+                clear(&mut conversation, writer)?;
                 continue;
             }
             None => {
@@ -98,7 +106,7 @@ fn main() -> Result<(), std::io::Error> {
             return Ok(());
         }
 
-        stdout.write_all(input.as_bytes())?;
+        writer.write_all(input.as_bytes())?;
 
         let user_input = Message {
             role: Role::User,
@@ -114,9 +122,92 @@ fn main() -> Result<(), std::io::Error> {
     }
 }
 
+fn main() -> Result<(), std::io::Error> {
+    let stdin = io::stdin();
+    let mut reader = stdin.lock();
+
+    let stdout = io::stdout();
+    let mut writer = stdout.lock();
+
+    run(&mut reader, &mut writer)
+}
+
 #[cfg(test)]
 mod tests {
+    use std::io::BufReader;
+
     use super::*;
+
+    #[test]
+    fn test_normal_conversation() {
+        let input = "hello\nexit";
+        let mut reader = BufReader::new(input.as_bytes());
+        let expected_output = "forge> hello\nforge> ";
+
+        let mut writer: Vec<u8> = Vec::new();
+
+        let result = run(&mut reader, &mut writer);
+
+        assert!(result.is_ok());
+        assert_eq!(writer, expected_output.as_bytes())
+    }
+
+    #[test]
+    fn test_non_empty_history_after_message() {
+        let input = "hello\nhow are you?\n/history";
+        let mut reader = BufReader::new(input.as_bytes());
+        let expected_output = "forge> hello\nforge> how are you?\nforge> user: hello\nforge: hello\nuser: how are you?\nforge: how are you?\nforge> ";
+
+        let mut writer: Vec<u8> = Vec::new();
+
+        let result = run(&mut reader, &mut writer);
+
+        assert!(result.is_ok());
+        assert_eq!(writer, expected_output.as_bytes())
+    }
+
+    #[test]
+    fn test_history_gets_cleared() {
+        let input = "hello\nhow are you?\n/clear\n/history";
+        let mut reader = BufReader::new(input.as_bytes());
+        let expected_output =
+            "forge> hello\nforge> how are you?\nforge> conversation cleared\nforge> forge> ";
+
+        let mut writer: Vec<u8> = Vec::new();
+
+        let result = run(&mut reader, &mut writer);
+
+        assert!(result.is_ok());
+        assert_eq!(writer, expected_output.as_bytes());
+    }
+
+    #[test]
+    fn test_blank_input() {
+        let input = "  \n";
+        let mut reader = BufReader::new(input.as_bytes());
+        let expected_output = "forge> forge> ";
+
+        let mut writer: Vec<u8> = Vec::new();
+
+        let result = run(&mut reader, &mut writer);
+
+        assert!(result.is_ok());
+        assert_eq!(writer, expected_output.as_bytes());
+    }
+
+    #[test]
+    fn test_eof() {
+        let input = "hello\n";
+        let mut reader = BufReader::new(input.as_bytes());
+        let expected_output = "forge> hello\nforge> ";
+
+        let mut writer: Vec<u8> = Vec::new();
+
+        let result = run(&mut reader, &mut writer);
+
+        assert!(result.is_ok());
+        assert_eq!(writer, expected_output.as_bytes());
+    }
 
     #[test]
     fn test_get_command_history() {
